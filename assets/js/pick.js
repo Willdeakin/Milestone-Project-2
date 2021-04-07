91bad393c24c041671f7a560996aa1c5280dcb17
Willdeakin <willdeakin@outlook.com>
2021-04-07 15:44:09 +0000

    attempted to add a filter system to buttons on click

---
+++ assets/js/pick.js
@@ -0,0 +1,7 @@
+var selectionArr = document.getElementsByClassName("selection-box");
+
+for (var i=0; i<selectionArr.length; i++){
+    $(this).removeClass("d-inline-block");
+    $(this).addClass("d-none");
+}
+
